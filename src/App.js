3cf24d6401fe54cb632f0a754ec211c3e16a1a14
narriteration <narriteration@gmail.com>
2017-03-29 14:44:03 -0700

    Step 1: set up App.js and Index.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 // Developer TODO: Define App component defintion here
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1>Hello World.</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
